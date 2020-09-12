2491ce7c91b466b7ef2b938f81ddf86630233122
Amr Abdel-Wahab <amr@orcas.io>
2020-09-12 16:21:12 +0200

    Initial Commit

---
+++ LetsTry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LetsTry
+//
+//  Created by Amr AbdelWahab on 9/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
