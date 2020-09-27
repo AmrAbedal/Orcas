6ea83f5d91d776f1086361dff28c0b5665edbe7c
Amr Abdel-Wahab <amr@orcas.io>
2020-09-27 17:40:53 +0200

    setup Screen list design

--- LetsTry/SubjectsView.swift
+++ LetsTry/SubjectsView.swift
@@ -16,9 +16,21 @@ struct SubjectsView: View {
         self.subjects = subjects
     }
     var body: some View {
+        ZStack {
+            Image("bg").resizable().scaledToFill().ignoresSafeArea()
         List(1..<100) { subject in
             SubjectCell(name:Subject(name: "subject \(subject)"))
-        }.navigationBarTitle("Subjects".localized,displayMode: .inline)
+        }.cornerRadius(10)
+        .padding(EdgeInsets(top: 100, leading: 20, bottom: 0, trailing: 20))
+        .navigationBarTitle("Subjects".localized,displayMode: .inline)
+        }
+        
     }
 }
 
+
+struct SubjectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SubjectsView()
+    }
+}
