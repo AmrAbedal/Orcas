7ea364d9505655a5c2e580eeb07f060f3a5dd896
Amr Abdel-Wahab <amr@orcas.io>
2020-09-27 13:18:44 +0200

    Orcas Explore Screen Design Screen

--- LetsTry/ContentView.swift
+++ LetsTry/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+  private func webinarsTapped() {
+        print("hello world")
+    }
+    private func tutorsTapped() {
+        
+    }
+    private func onlineTutoringTapped() {
+        
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("bg").resizable().scaledToFill().ignoresSafeArea()
+            VStack(alignment:.leading,spacing:18) {
+                Text("Explore").foregroundColor(.white).font(.custom("SFUIDisplay-Semibold", size: 26))
+                Text("☀️ Good Morning, Nadya").foregroundColor(.white).font(.custom("SFUIDisplay-Medium", size: 15))
+                Text("Who are you\n looking for\n today ?").foregroundColor(.white).font(.custom("SFUIDisplay-Heavy", size: 38))
+                WebinarView(action: webinarsTapped)
+                TutorsView(action: tutorsTapped)
+                OnlineTutoring(action: onlineTutoringTapped)
+            }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading).padding(EdgeInsets(top: 68, leading: 16, bottom: 0, trailing: 0))
+        }
     }
 }
 
---
+++ LetsTry/TutorsView.swift
@@ -0,0 +1,24 @@
+//
+//  TutorsView.swift
+//  LetsTry
+//
+//  Created by Amr AbdelWahab on 9/27/20.
+//
+
+import Foundation
+import SwiftUI
+
+struct TutorsView: View {
+    var action: ()->()
+    init(action: @escaping ()->() ) {
+        self.action = action
+    }
+    var body: some View {
+        Button(action: action ) {
+            Image("ic_home_discover")
+            Text("Tutors").foregroundColor(Color.init("ServiceColor")).font(.custom("SFUIDisplay-Bold", size: 21))
+            Image("ic_home_arrow")
+        }.padding(10).background(Color.white).cornerRadius(25)
+        
+    }
+}
